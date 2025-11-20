fbc66808a20cb6f946e32d68b66a973bba6e7fa9
polo <lg550055@hotmail.com>
2025-11-20 11:36:54 -0800

    Removes deprecated mongo connection params

--- server.js
+++ server.js
@@ -5,10 +5,8 @@ require('dotenv').config();
 const MONGODB_URI = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 4000;
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log('Connected to MongoDB'))
+mongoose.connect(MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
 // Define Mongoose schema
